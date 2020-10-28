ba56480e163a66c327476b3ad75d412b8987f17f
Nelson Nadar <nelsonnadar@nelsons-mbp.home>
2020-10-28 11:07:45 +0000

    first commit

---
+++ src/test/java/io/nelson/nadar/configclientapp/ConfigClientAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nelson.nadar.configclientapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
